89ad530aa6fc8b8052cc449e983a4b710b4227b7
Kamill Sokol <ksokol@communicode.de>
2015-12-05 14:53:13 +0100

    migrated CalendarTests to Groovy in order to assert xml/html request/response efficiently

--- src/test/groovy/carldav/util/builder/GeneralResponse.groovy
+++ src/test/groovy/carldav/util/builder/GeneralResponse.groovy
@@ -12,4 +12,19 @@ public class GeneralResponse {
                                                     <D:error xmlns:cosmo="http://osafoundation.org/cosmo/DAV" xmlns:D="DAV:">
                                                         <cosmo:internal-server-error></cosmo:internal-server-error>
                                                     </D:error>"""
+
+    public static String RESOURCE_MUST_BE_NULL = """\
+                                                    <D:error xmlns:cosmo="http://osafoundation.org/cosmo/DAV" xmlns:D="DAV:">
+                                                        <D:resource-must-be-null>A resource exists at the request URI</D:resource-must-be-null>
+                                                    </D:error>"""
+
+    public static String NOT_FOUND = """\
+                                        <D:error xmlns:cosmo="http://osafoundation.org/cosmo/DAV" xmlns:D="DAV:">
+                                            <cosmo:not-found></cosmo:not-found>
+                                        </D:error>"""
+
+    public static String NOT_SUPPORTED_PRIVILEGE = """\
+                                                       <D:error xmlns:cosmo="http://osafoundation.org/cosmo/DAV" xmlns:D="DAV:">
+                                                            <D:not-supported-privilege>No unprotected ACEs are supported on this resource</D:not-supported-privilege>
+                                                       </D:error>"""
 }

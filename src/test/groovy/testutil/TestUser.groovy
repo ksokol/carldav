9d0e1385c54a17300f26f89455251f3980a2ecf3
Kamill Sokol <ksokol@communicode.de>
2015-12-06 15:13:44 +0100

    removed enum TestUser

--- src/test/groovy/testutil/TestUser.groovy
+++ src/test/groovy/testutil/TestUser.groovy
@@ -5,5 +5,9 @@ package testutil
 class TestUser {
 
     static final String USER01 = "test01@localhost.de"
+    static final String USER01_PASSWORD = "test"
+
+    static final String UNKNOWN = "unknown"
+    static final String UNKNOWN_PASSWORD = "unknown"
 
 }

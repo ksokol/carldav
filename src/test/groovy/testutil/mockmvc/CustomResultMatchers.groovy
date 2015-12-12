bc8bf63544e1ccd90a2c0555358a37feded69821
Kamill Sokol <dev@sokol-web.de>
2015-12-12 14:23:54 +0100

    added test to CalendarTests which asserts that an event is available under ressource /dav/{email}/calendar/{uid}.ics

--- src/test/groovy/testutil/mockmvc/CustomResultMatchers.groovy
+++ src/test/groovy/testutil/mockmvc/CustomResultMatchers.groovy
@@ -6,6 +6,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers
 import testutil.xmlunit.XmlMatcher
 
 import static org.hamcrest.CoreMatchers.is
+import static org.hamcrest.text.IsEqualIgnoringWhiteSpace.equalToIgnoringWhiteSpace
 import static org.springframework.http.HttpHeaders.*
 import static org.springframework.http.MediaType.TEXT_HTML_VALUE
 import static org.springframework.http.MediaType.TEXT_XML_VALUE
@@ -27,6 +28,10 @@ public class CustomResultMatchers {
         return MockMvcResultMatchers.content().string(content);
     }
 
+    public static ResultMatcher text(String content) {
+        return MockMvcResultMatchers.content().string(equalToIgnoringWhiteSpace(content));
+    }
+
     public static ResultMatcher etag(Matcher<? super String> m) {
         return header().string(ETAG, m);
     }
@@ -45,5 +50,9 @@ public class CustomResultMatchers {
 
     public static ResultMatcher textHtmlContentType() {
         return contentType(is(TEXT_HTML_VALUE + "; charset=UTF-8"));
+    }
+
+    public static ResultMatcher textCalendarContentType() {
+        return contentType(is("text/calendar; charset=UTF-8"));
     }
 }

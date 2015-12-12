3e7f4158b723a04cca1b0d7e68651214259cb38d
Kamill Sokol <dev@sokol-web.de>
2015-12-12 11:39:55 +0100

    removed unused code from FreeBusyUtils

--- src/main/java/org/unitedinternet/cosmo/calendar/FreeBusyUtils.java
+++ src/main/java/org/unitedinternet/cosmo/calendar/FreeBusyUtils.java
@@ -15,25 +15,19 @@
  */
 package org.unitedinternet.cosmo.calendar;
 
-import java.util.Iterator;
-import java.util.List;
-
-import org.unitedinternet.cosmo.util.VersionFourGenerator;
-
-
-
 import net.fortuna.ical4j.model.Parameter;
 import net.fortuna.ical4j.model.Period;
 import net.fortuna.ical4j.model.PeriodList;
 import net.fortuna.ical4j.model.Property;
 import net.fortuna.ical4j.model.PropertyList;
-import net.fortuna.ical4j.model.TimeZone;
 import net.fortuna.ical4j.model.component.VFreeBusy;
 import net.fortuna.ical4j.model.parameter.FbType;
-import net.fortuna.ical4j.model.property.DtEnd;
-import net.fortuna.ical4j.model.property.DtStart;
 import net.fortuna.ical4j.model.property.FreeBusy;
 import net.fortuna.ical4j.model.property.Uid;
+import org.unitedinternet.cosmo.util.VersionFourGenerator;
+
+import java.util.Iterator;
+import java.util.List;
 
 /**
  * 
@@ -42,75 +36,7 @@ import net.fortuna.ical4j.model.property.Uid;
 public class FreeBusyUtils {
     
     private static final VersionFourGenerator UUID_GENERATOR = new VersionFourGenerator();
-    
-    /**
-        A VFREEBUSY component overlaps a given time range if the condition
-        for the corresponding component state specified in the table below
-        is satisfied.  The conditions depend on the presence in the
-        VFREEBUSY component of the DTSTART and DTEND properties, and any
-        FREEBUSY properties in the absence of DTSTART and DTEND.  Any
-        DURATION property is ignored, as it has a special meaning when
-        used in a VFREEBUSY component.
-    
-        When only FREEBUSY properties are used, each period in each
-        FREEBUSY property is compared against the time range, irrespective
-        of the type of free busy information (free, busy, busy-tentative,
-        busy-unavailable) represented by the property.
-    
-    
-        +------------------------------------------------------+
-        | VFREEBUSY has both the DTSTART and DTEND properties? |
-        |   +--------------------------------------------------+
-        |   | VFREEBUSY has the FREEBUSY property?             |
-        |   |   +----------------------------------------------+
-        |   |   | Condition to evaluate                        |
-        +---+---+----------------------------------------------+
-        | Y | * | (start <= DTEND) AND (end > DTSTART)         |
-        +---+---+----------------------------------------------+
-        | N | Y | (start <  freebusy-period-end) AND           |
-        |   |   | (end   >  freebusy-period-start)             |
-        +---+---+----------------------------------------------+
-        | N | N | FALSE                                        |
-        +---+---+----------------------------------------------+
-     *
-     * @param freeBusy comoponent to test
-     * @param period period to test against
-     * @param tz timezone to use for floating times
-     * @return true if component overlaps specified range, false otherwise
-     */
-    public static boolean overlapsPeriod(VFreeBusy freeBusy, Period period, TimeZone tz){
-        
-        DtStart start = freeBusy.getStartDate();
-        DtEnd end = freeBusy.getEndDate();
-         
-        if (start != null && end != null) {
-            InstanceList instances = new InstanceList();
-            instances.setTimezone(tz);
-            instances.addComponent(freeBusy, period.getStart(),period.getEnd());
-            return instances.size() > 0;
-        }
-        
-        PropertyList props = freeBusy.getProperties(Property.FREEBUSY);
-        if (props.size()==0) {
-            return false;
-        }
-        
-        Iterator<FreeBusy> it = props.iterator();
-        while(it.hasNext()) {
-            FreeBusy fb = it.next();
-            PeriodList periods = fb.getPeriods();
-            Iterator<Period> periodIt = periods.iterator();
-            while(periodIt.hasNext()) {
-                Period fbPeriod = periodIt.next();
-                if(fbPeriod.intersects(period)) {
-                    return true;
-                }
-            }
-        }
-        
-        return false;
-    }
-    
+
     /**
      * Merge multiple VFREEBUSY components into a single VFREEBUSY
      * component.

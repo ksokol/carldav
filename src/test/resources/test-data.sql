fe3e8bd62be3cafe9920d9902aadf72879429254
Kamill Sokol <dev@sokol-web.de>
2015-12-19 17:18:51 +0100

    removed BooleanAttribute and related code

--- src/test/resources/test-data.sql
+++ src/test/resources/test-data.sql
@@ -6,9 +6,6 @@ INSERT INTO item (itemtype, id, createdate, etag, modifydate, clientcreatedate, 
 INSERT INTO item (itemtype, id, createdate, etag, modifydate, clientcreatedate, clientmodifieddate, displayname, itemname, uid, version, lastmodification, lastmodifiedby, needsreply, sent, isautotriage, triagestatuscode, triagestatusrank, icaluid, hasmodifications, ownerid, modifiesitemid) VALUES ('collection', 2, 1448140260072, 'NVy57RJot0LhdYELkMDJ9gQZjOM=', 1448140260072, null, null, 'calendarDisplayName', 'calendar', 'a172ed34-0106-4616-bb40-a416a8305465', 0, null, null, null, null, null, null, null, null, null, 2, null);
 INSERT INTO item (itemtype, id, createdate, etag, modifydate, clientcreatedate, clientmodifieddate, displayname, itemname, uid, version, lastmodification, lastmodifiedby, needsreply, sent, isautotriage, triagestatuscode, triagestatusrank, icaluid, hasmodifications, ownerid, modifiesitemid) VALUES ('homecollection', 3, 1448140260056, null, 1448140260056, null, null, null, 'test02@localhost.de', '1e359448-1ee0-4151-872d-eea0ee462bc6', 0, null, null, null, null, null, null, null, null, null, 3, null);
 
-INSERT INTO attribute (attributetype, id, createdate, etag, modifydate, localname, namespace, booleanvalue, textvalue, intvalue, stringvalue, itemid) VALUES ('string', 1, 1448140260077, '', 1448140260077, 'color', 'org.unitedinternet.cosmo.model.CalendarCollectionStamp', null, null, null, '#f0f0f0', 2);
-INSERT INTO attribute (attributetype, id, createdate, etag, modifydate, localname, namespace, booleanvalue, textvalue, intvalue, stringvalue, itemid) VALUES ('boolean', 2, 1448140260079, '', 1448140260079, 'visibility', 'org.unitedinternet.cosmo.model.CalendarCollectionStamp', true, null, null, null, 2);
-
 INSERT INTO stamp (stamptype, id, createdate, etag, modifydate, itemid) VALUES ('calendar', 1, 1448140260087, '', 1448140260087, 2);
 
 INSERT INTO collection_item (createdate, itemid, collectionid) VALUES (1448140260072, 2, 1);

633fa4284890524f0b83b1e8d455304318df0f24
Kamill Sokol <dev@sokol-web.de>
2016-01-23 16:09:40 +0100

    removed unused constructor from ItemNotFoundException

--- src/main/java/org/unitedinternet/cosmo/dao/ItemNotFoundException.java
+++ src/main/java/org/unitedinternet/cosmo/dao/ItemNotFoundException.java
@@ -21,15 +21,8 @@ import org.springframework.dao.DataRetrievalFailureException;
  * @author rletness
  */
 public class ItemNotFoundException extends DataRetrievalFailureException {
-    /**
-     */
+
     public ItemNotFoundException(String message) {
         super(message);
-    }
-
-    /**
-     */
-    public ItemNotFoundException(String message, Throwable cause) {
-        super(message, cause);
     }
 }

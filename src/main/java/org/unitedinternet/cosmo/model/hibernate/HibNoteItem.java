7ec411659e5b56e4b661424645fc4a2177f1b8d3
Kamill Sokol <dev@sokol-web.de>
2016-02-13 12:47:51 +0100

    removed unused methods from HibNoteItem

--- src/main/java/org/unitedinternet/cosmo/model/hibernate/HibNoteItem.java
+++ src/main/java/org/unitedinternet/cosmo/model/hibernate/HibNoteItem.java
@@ -16,13 +16,7 @@
 package org.unitedinternet.cosmo.model.hibernate;
 
 import net.fortuna.ical4j.model.Calendar;
-import net.fortuna.ical4j.model.DateTime;
-import net.fortuna.ical4j.model.Dur;
-import net.fortuna.ical4j.model.component.VEvent;
-import net.fortuna.ical4j.model.property.DtEnd;
-import net.fortuna.ical4j.model.property.DtStart;
 import org.hibernate.annotations.Target;
-import org.unitedinternet.cosmo.calendar.ICalendarUtils;
 
 import java.nio.charset.Charset;
 import java.util.Date;
@@ -80,67 +74,12 @@ public class HibNoteItem extends HibICalendarItem {
         this.remindertime = new Date(remindertime.getTime());
     }
 
-    public Calendar getTaskCalendar() {
-        return getCalendar();
-    }
-    
     public void setTaskCalendar(Calendar calendar) {
         setCalendar(calendar);
     }
 
     public void setStartDate(final Date startDate) {
         this.startDate = startDate;
-    }
-
-    public net.fortuna.ical4j.model.Date getStartDate() {
-        //return getEventException().getStartDate();
-        VEvent event = (VEvent) getCalendar().getComponents("VEVENT").get(0);
-        if(event==null) {
-            return null;
-        }
-
-        DtStart dtStart = event.getStartDate();
-        if (dtStart == null) {
-            return null;
-        }
-        return dtStart.getDate();
-    }
-
-    private Dur getDuration() {
-      //  return ICalendarUtils.getDuration(getExceptionEvent());
-        return ICalendarUtils.getDuration((VEvent) getCalendar().getComponents("VEVENT").get(0));
-    }
-
-    public net.fortuna.ical4j.model.Date getEndDate() {
-       // VEvent event = getExceptionEvent();
-        VEvent event = (VEvent) getCalendar().getComponents("VEVENT").get(0);
-        if(event==null) {
-            return null;
-        }
-        DtEnd dtEnd = event.getEndDate(false);
-        // if no DTEND, then calculate endDate from DURATION
-        if (dtEnd == null) {
-            net.fortuna.ical4j.model.Date startDate = getStartDate();
-            Dur duration = this.getDuration();
-
-            // if no DURATION, then there is no end time
-            if(duration==null) {
-                return null;
-            }
-
-            net.fortuna.ical4j.model.Date endDate = null;
-            if(startDate instanceof DateTime) {
-                endDate = new DateTime(startDate);
-            }
-            else {
-                endDate = new net.fortuna.ical4j.model.Date(startDate);
-            }
-
-            endDate.setTime(duration.getTime(startDate).getTime());
-            return endDate;
-        }
-
-        return dtEnd.getDate();
     }
 
     @Override

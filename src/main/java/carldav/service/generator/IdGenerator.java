18271c8ca53aef96df47c28fa0c965fd3fe027e2
Kamill Sokol <dev@sokol-web.de>
2016-03-13 18:09:29 +0100

    removed IdGenerator

--- src/main/java/carldav/service/generator/IdGenerator.java
+++
@@ -1,8 +0,0 @@
-package carldav.service.generator;
-
-/**
- * @author Kamill Sokol
- */
-public interface IdGenerator {
-    String nextStringIdentifier();
-}
--- src/main/java/org/unitedinternet/cosmo/model/hibernate/AuditableObjectInterceptor.java
+++ src/main/java/org/unitedinternet/cosmo/model/hibernate/AuditableObjectInterceptor.java
@@ -15,34 +15,23 @@
  */
 package org.unitedinternet.cosmo.model.hibernate;
 
-import java.io.Serializable;
-import java.lang.reflect.Field;
-import java.util.Date;
-
-import carldav.service.generator.IdGenerator;
+import carldav.service.time.TimeService;
 import org.hibernate.EmptyInterceptor;
 import org.hibernate.type.Type;
 import org.springframework.util.Assert;
-import carldav.service.time.TimeService;
-import org.springframework.util.ReflectionUtils;
 
-/**
- * Hibernate Interceptor that updates creationDate, modifiedDate,
- * and etag each time an AuditableObject is saved/updated.
- */
+import java.io.Serializable;
+import java.util.Date;
+
 public class AuditableObjectInterceptor extends EmptyInterceptor {
 
     private static final long serialVersionUID = 2206186604411196082L;
 
     private final TimeService timeService;
-    private final IdGenerator idGenerator;
 
-    public AuditableObjectInterceptor(final TimeService timeService, IdGenerator idGenerator) {
+    public AuditableObjectInterceptor(final TimeService timeService) {
         Assert.notNull(timeService, "timeService is null");
-        Assert.notNull(idGenerator, "idGenerator is null");
         this.timeService = timeService;
-        this.idGenerator = idGenerator;
-
     }
 
     @Override
@@ -53,38 +42,20 @@ public class AuditableObjectInterceptor extends EmptyInterceptor {
 
     @Override
     public boolean onSave(Object object, Serializable id, Object[] state, String[] propertyNames, Type[] types) {
-        
         if(! (object instanceof HibAuditableObject)) {
             return false;
         }
-        
-        // Set new modifyDate so that calculateEntityTag()
-        // has access to it
+
         HibAuditableObject ao = (HibAuditableObject) object;
         Date curDate = timeService.getCurrentTime();
-        ao.setModifiedDate(curDate);
-        
-        // initialize modifiedDate, creationDate and entityTag
+
         for ( int i=0; i < propertyNames.length; i++ ) {
             if ("modifiedDate".equals(propertyNames[i])) {
                 state[i] = curDate;
             } else if("etag".equals( propertyNames[i] )) {
                 state[i] = ao.calculateEntityTag();
-            } else if("uid".equals(propertyNames[i])) {
-                setUid(state, ao, i);
             }
         }
         return true;
     }
-
-    //TODO remove me as soon as HibCollectionItem does not derive from HibItem anymore
-    private void setUid(Object[] state, HibAuditableObject ao, int i) {
-        final Field field = ReflectionUtils.findField(HibItem.class, "uid");
-        ReflectionUtils.makeAccessible(field);
-        final Object field1 = ReflectionUtils.getField(field, ao);
-        if(field1 == null) {
-            state[i] = idGenerator.nextStringIdentifier();
-        }
-    }
-
 }

5bb84b70b09be4ffe8d41fdb0ff87ba3e2c40f75
Kamill Sokol <dev@sokol-web.de>
2016-03-31 21:19:37 +0200

    added tests for AdminUserCreator

---
+++ src/test/groovy/carldav/bootstrap/AdminUserCreatorTest.groovy
@@ -0,0 +1,46 @@
+package carldav.bootstrap
+
+import carldav.entity.User
+import carldav.repository.UserRepository
+import org.junit.Test
+import org.springframework.security.authentication.encoding.PasswordEncoder
+
+import static org.mockito.Matchers.anyString
+import static org.mockito.Mockito.*
+
+/**
+ * @author Kamill Sokol
+ */
+class AdminUserCreatorTest {
+
+    public static final String IGNORE = "ignore"
+
+    private UserRepository userRepository = mock(UserRepository.class);
+    private PasswordEncoder passwordEncoder = mock(PasswordEncoder.class);
+    private User user = mock(User.class)
+
+    private AdminUserCreator uut = new AdminUserCreator(userRepository, passwordEncoder, IGNORE, IGNORE)
+
+    @Test
+    void adminPasswordEqual() {
+        when(userRepository.findByEmailIgnoreCase(IGNORE)).thenReturn(user)
+        when(user.getPassword()).thenReturn(IGNORE)
+
+        uut.onApplicationEvent(null)
+
+        verify(user, never()).setPassword(anyString())
+        verify(userRepository, never()).save(user)
+    }
+
+    @Test
+    void adminPasswordNotEqual() {
+        when(userRepository.findByEmailIgnoreCase(IGNORE)).thenReturn(user)
+        when(user.getPassword()).thenReturn("different")
+        when(passwordEncoder.encodePassword(IGNORE, null)).thenReturn("encoded")
+
+        uut.onApplicationEvent(null)
+
+        verify(user).setPassword("encoded")
+        verify(userRepository).save(user)
+    }
+}

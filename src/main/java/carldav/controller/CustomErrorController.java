e7cb9cb02b0f952f1d37e7d3b7fb0a777be1e71c
Kamill Sokol <dev@sokol-web.de>
2022-04-02 17:55:00 +0200

    chore: Update Spring Boot 2.4.12 => 2.5.12

--- src/main/java/carldav/controller/CustomErrorController.java
+++ src/main/java/carldav/controller/CustomErrorController.java
@@ -2,38 +2,28 @@ package carldav.controller;
 
 import carldav.exception.resolver.ResponseUtils;
 import org.springframework.boot.autoconfigure.web.servlet.error.AbstractErrorController;
+import org.springframework.boot.web.error.ErrorAttributeOptions;
 import org.springframework.boot.web.servlet.error.ErrorAttributes;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.unitedinternet.cosmo.dav.CosmoDavException;
 
-import java.io.IOException;
-import java.util.Map;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
-/**
- * @author Kamill Sokol
- */
 @RestController
 public class CustomErrorController extends AbstractErrorController {
 
-    public CustomErrorController(final ErrorAttributes errorAttributes) {
+    public CustomErrorController(ErrorAttributes errorAttributes) {
         super(errorAttributes);
     }
 
     @RequestMapping("error")
-    public void test(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        final Map<String, Object> errorAttributes = getErrorAttributes(request, false);
-        final HttpStatus status = getStatus(request);
-        final String message = errorAttributes.get("message").toString();
+    public void error(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        var errorAttributes = getErrorAttributes(request, ErrorAttributeOptions.defaults());
+        var status = getStatus(request);
+        var message = errorAttributes.get("message").toString();
         ResponseUtils.sendDavError(new CosmoDavException(status.value(), message), response);
-    }
-
-    @Override
-    public String getErrorPath() {
-        return null;
     }
 }
